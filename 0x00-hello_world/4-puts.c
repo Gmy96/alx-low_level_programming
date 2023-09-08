c59732ae3644198025b913335f50ba743051d7dd
GMY96 <ag199612@gmail.com>
2023-09-08 07:06:49 -0700

    My C - Hello, World Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+ * main - Entry point
+ *
+ * Description: C program that prints with the function puts
+ *
+ * Return: Always 0 to success
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
