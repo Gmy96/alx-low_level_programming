5a6b99f6b9970283d27fb5ee5134c086a1c4013a
GMY96 <ag199612@gmail.com>
2023-09-08 07:12:02 -0700

    My C - Hello, World Task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that Use the function printf
+ *
+ * Return: Always 0 to success
+*/
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
