2ceeb3fae011f77b8545ce247a1417dd41966a82
GMY96 <ag199612@gmail.com>
2023-09-12 08:52:21 -0700

    My C - Variables, if, else, while Task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * Description: prints the alphabet in lowercase, and then in uppercase
+ *    * Return: Always 0
+ *    */
+
+int main(void)
+{
+	    int x = 97;
+	        int y = 65;
+		    
+		    while (x <= 122)
+			        {
+					        putchar(x);
+						        x++;
+							    }
+		        
+		        while (y <= 90)
+				    {
+					            putchar(y);
+						            y++;
+							        }
+			    
+			    putchar('$');
+			        return (0);
+}
