08e91e98e85a931aac64312729fdc4c2d61b6640
GMY96 <ag199612@gmail.com>
2023-09-14 03:09:11 -0700

    My C - Variables, if, else, while Task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of two digits
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = x + 1; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (x == '8')
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	
+	putchar('\n');
+	return (0);
+}
