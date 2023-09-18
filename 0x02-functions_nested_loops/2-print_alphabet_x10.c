201079f7713678ecb952b39bf9b05ba226a9ee7b
root <root@DESKTOP-269MANA.localdomain>
2023-09-19 00:45:04 +0300

    My C - Functions, nested loops Task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/***/
+
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 97; y <= 122; y++)
+		{
+			_putchar(y);
+		}
+        _putchar('\n');
+	}
+}
