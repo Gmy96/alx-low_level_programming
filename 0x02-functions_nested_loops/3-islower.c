83e61d39da2ec85a6f2c92a1ad24e1d9411729a8
root <root@DESKTOP-269MANA.localdomain>
2023-09-19 01:11:44 +0300

    My C - Functions, nested loops Task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,16 +13,13 @@ int _islower(int c)
 {
 	int x;
 
-	for (x = 97; x <= 122; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		if (x == c)
 		{
 			return (1);
 		}
-		else
-		{
-			return (0);
-		}
+		return (0);
 	}
 }
 
--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * test_islower - Test the _islower function
- *
- * @n: Number to pass to _islower function
- */
+* test_islower - Test the _islower function
+*
+* @n: Number to pass to _islower function
+*/
 void test_islower(int n)
 {
 	int r;
@@ -15,10 +15,10 @@ void test_islower(int n)
 }
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 	test_islower('H');
