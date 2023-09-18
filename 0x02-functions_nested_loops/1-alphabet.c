e2e750321e1993198e04821243b9aa3719acfbd2
root <root@DESKTOP-269MANA.localdomain>
2023-09-18 23:59:21 +0300

    My C - Functions, nested loops Task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - function that prints the alphabet , in lowercase
- *
- * Return: Always 0
+* print_alphabet - function that prints the alphabet , in lowercase
+*
+* Return: Always 0
 */
 
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar(\n);
+	return (0);
+}
