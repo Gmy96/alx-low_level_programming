1d58f547552cfc22454d7068fc03de6cd1364533
root <root@DESKTOP-269MANA.localdomain>
2023-09-18 21:36:58 +0300

    My C - Functions, nested loops Task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *
- *Description: program that prints _putchar
- *
- *Return: Always 0
- */
+*main - Entry point
+*
+*Description: program that prints _putchar
+*
+*Return: Always 0
+*/
 
 int main(void)
 {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return (0);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
 }
