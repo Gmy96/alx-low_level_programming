250a9211e3989891ce3fba979d32384ebff865e8
root <root@DESKTOP-269MANA.localdomain>
2023-09-19 19:18:39 +0300

    My C - Functions, nested loops Task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/***/
+
+int _abs(int)
+{
+    int x;
+
+    if (x >= 0)
+    {
+        return (x);
+    }
+
+    else
+    {
+        return (x * -1);
+    }
+}
