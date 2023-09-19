e9782562c8c61d45993164af67950ecd68c4ff18
root <root@DESKTOP-269MANA.localdomain>
2023-09-19 18:54:42 +0300

    My C - Functions, nested loops Task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/***/
+
+int print_sign(int n)
+{
+    if (n > 48)
+    {
+        _putchar('+');
+        return (1);
+    }
+
+    if else (n == 48)
+    {
+        _putchar('0');
+        return (0);
+    }
+
+    else
+    {
+        _putchar('-');
+        return (-1);
+    }
+}
