92f98f7f2f0e205c31c75cf33e5cbff37aa6fa0d
root <root@DESKTOP-269MANA.localdomain>
2023-09-20 00:45:29 +0300

    My C - Functions, nested loops Task

--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -2,33 +2,31 @@
 
 int main(void)
 {
-	int x;
-	int y;
-	int z;
-	int q;
+	int x, y, z;
 
-	for (x = '0'; x <= '2'; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (y = '0'; y <= '9'; y++)
+		putchar('0');
+
+		for (y = 1; y <= 9; y++)
 		{
-			for (z = '0'; z <= '5'; z++)
+			putchar(',');
+			putchar(' ');
+
+		z = x * y;
+
+			if (z <= 9)
 			{
-				for (q = '0'; q <= '9'; q++)
-				{
-					if (y >= '4')
-					{
-						break;
-					}
-					putchar(x);
-					putchar(y);
-					putchar(':');
-					putchar(z);
-					putchar(q);
-					putchar('\n');
-				}
+				putchar(' ');
 			}
+
+			else
+			{
+				putchar((z / 10) + '0');
+			}
+
+			putchar((z % 10) + '0');
 		}
+        putchar('\n');
 	}
-	putchar('\n');
-	return (0);
 }
