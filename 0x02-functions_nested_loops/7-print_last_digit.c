2c214a7a887f7fa5ec498d983bb4a5e95d5ffe37
root <root@DESKTOP-269MANA.localdomain>
2023-09-19 19:54:44 +0300

    My C - Functions, nested loops Task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
-/**/
+/**
+ * print_last_digit - function that prints the last digit of a number
+ *
+ * @x: the intger
+ *
+ * Return: the last digit
+*/
 
 int print_last_digit(int x)
 {
