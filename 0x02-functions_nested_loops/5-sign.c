7d31ff2cdb0b6db1dfcca28dd828006dee65ece1
root <root@DESKTOP-269MANA.localdomain>
2023-09-19 18:59:26 +0300

    My C - Functions, nested loops Task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
-/***/
+/**
+ * print_sign - function that prints the sign of a number
+ *
+ * @n: The number
+ *
+ * Return: 1 if greater than zero, -1 if is zero, 0 if is less than zero
+*/
 
 int print_sign(int n)
 {
