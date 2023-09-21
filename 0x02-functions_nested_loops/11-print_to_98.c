59ddee052ad1d775a8b63e2b0b51c0ce1df539cb
root <root@DESKTOP-269MANA.localdomain>
2023-09-21 23:03:43 +0300

    My Functions, nested loops Task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_to_98(int n)
+{
+    int x;
+
+    for (x = n; x > 98; x--)
+    {
+        printf("%d,", x);
+        printf("\n", x);
+    }
+
+    for (x = n; x < 98; x++)
+    {
+        printf("%d,", x);
+        printf("\n", x);
+    }
+}
