13e3214d574c293d5b9b20df4c66462c679f53b3
root <root@DESKTOP-269MANA.localdomain>
2023-09-23 21:57:17 +0300

    My C - Functions, nested loops Task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description: list all the natural numbers are multiples of 3 or 5
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x;
+
+	int Sum;
+
+	for (x = 3; x < 1024; x++)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			Sum = Sum = x;
+		}
+	}
+
+	printf("%d\n", Sum);
+
+	return (0);
+}
