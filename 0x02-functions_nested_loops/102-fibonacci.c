899b044819069a823dddbf9d203048f41d671206
root <root@DESKTOP-269MANA.localdomain>
2023-09-23 22:11:08 +0300

    My C - Functions, nested loops Task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Description: prints the first 50 Fibonacci numbers, starting with 1 and 2
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int x = 1;
+
+	int y = 2;
+
+	int Sum;
+
+	printf("1, 2");
+
+	for (x = 3; x <= 50; x++)
+	{
+		Sum = x + y;
+		printf("%d, ", Sum);
+		x = y;
+		y = Sum;
+	}
+
+	printf("\n");
+
+	return (0);
+}
