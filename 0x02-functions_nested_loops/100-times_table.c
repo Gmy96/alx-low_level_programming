b02b8fc2e6126f37c98197f3f6194eeadc130686
root <root@DESKTOP-269MANA.localdomain>
2023-09-23 21:02:23 +0300

    My C - Functions, nested loops Task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_times_table -  function that prints the n times table, starting with 0
+*
+* @n: The intger of times table
+*
+* Return: No return
+*/
+
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('0');
+
+			for (y = 1; y <= n; y++)
+			{
+				_putchar(',');
+			    _putchar(' ');
+
+				z = x * y;
+
+				if (z <= 9)
+				{
+					_putchar(' ');
+				}
+
+				if (z <= 99)
+				{
+					_putchar(' ');
+				}
+
+				if (z >= 100)
+				{
+					_putchar((z / 100) + '0');
+					_putchar((z / 10) % 10 + '0');
+				}
+
+				else if (z <= 99 && z >= 10)
+				{
+					_putchar((z / 10) + '0');
+				}
+				_putchar((z % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
