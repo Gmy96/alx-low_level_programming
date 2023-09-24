88bcb9f3248a2be90934df1b8a23ec027dfb1744
root <root@DESKTOP-269MANA.localdomain>
2023-09-24 22:33:31 +0300

    My More functions, more nested loops Task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-/***/
+/**
+ * _isupper - function that checks for uppercase character
+ *
+ * @c: The Character
+ *
+ * Return: 1 for uppercase character and 0 for otherwise
+*/
 
 int _isupper(int c)
 {
