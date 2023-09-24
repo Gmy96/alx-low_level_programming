cde9ec86ceec73c36dd3136e8af8666044621fcc
root <root@DESKTOP-269MANA.localdomain>
2023-09-24 23:12:07 +0300

    My More functions, more nested loops Task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,26 +1,24 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
- * 
- * 
+* print_most_numbers - prints the numbers from 0 to 9 without printing 2 and 4
+*
+* Return: No return
 */
 
 void print_most_numbers(void)
 {
-    int x;
+	int x;
 
-    for (x = 48; x <= 57; x++)
-    {
-        if (x == 2 || x == 4)
-        {
-            break;
-        }
+	for (x = 48; x <= 57; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			break;
+		}
 
-        _putchar(x);
-    }
+		_putchar(x);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
