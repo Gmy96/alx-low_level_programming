3304af352b7dc833a59d945c76e48269d5f7d3ee
root <root@DESKTOP-269MANA.localdomain>
2023-09-24 22:56:20 +0300

    My More functions, more nested loops Task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
 * print_numbers - function that prints the numbers, from 0 to 9
 *
-* Return: No return
+* Return: Always 0
 */
 
 void print_numbers(void)
@@ -14,6 +14,8 @@ void print_numbers(void)
 	{
 		_putchar(x);
 	}
+
+	return (0);
 
 	_putchar('\n');
 }
