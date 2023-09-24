396808ab5d3d90d7c42e28a6832b10c6953a571d
root <root@DESKTOP-269MANA.localdomain>
2023-09-24 23:20:11 +0300

    My More functions, more nested loops Task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - function that prints 10 times the numbers, from 0 to 14
+*
+* Return: No return
+*/
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = '0'; y <= '14'; y++)
+		{
+			_putchar(y);
+			_putchar('\n');
+		}
+		_putchar('\n')
+	}
+}
