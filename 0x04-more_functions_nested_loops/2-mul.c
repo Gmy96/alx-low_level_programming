3425e195fb0dd1306fc06a8650fa738697984fae
root <root@DESKTOP-269MANA.localdomain>
2023-09-24 22:50:41 +0300

    My More functions, more nested loops Task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+/**
+* mul - function that multiplies two integers
+*
+* @a: The fisrt integer
+*
+* @b: The second intger
+*
+* Return: The multiplie of the two integers
+*/
+
+int mul(int a, int b)
+{
+	int z;
+
+	z = a * b;
+
+	_putchar(z);
+
+	return (z);
+}
