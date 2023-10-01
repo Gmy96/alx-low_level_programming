e13587e6fe65e057eb2dadf3b9125f3b3a57b0ec
GMY96 <ag199612@gmail.com>
2023-10-02 00:06:15 +0300

    My C - Recursion Task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+# include "main.h"
+
+/**
+ * print_rev_recursion - function that prints a string in reverse
+ *
+ * @s: The string
+*/
+
+void _print_rev_recursion(char *s)
+{
+    if (*s)
+	{
+		_puts_recursion(s + 1);
+        _putchar(*s);
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
