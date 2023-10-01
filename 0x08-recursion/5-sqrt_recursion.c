f7c14e2795672c0c2949c55c315b43f7bcc2d3f5
GMY96 <ag199612@gmail.com>
2023-10-02 01:12:45 +0300

    My C - Recursion Task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,42 @@
 #include "main.h"
 
-/***/
+/**
+ * square - find the square root
+ *
+ * @n: The intger
+ *
+ * @y: The intger to test
+ *
+ * Return: The square root
+*/
+
+int square(int n, int y)
+{
+	if (y * y == n)
+	{
+		return (y);
+	}
+
+	else if (y * y < n)
+	{
+		return (square(n, y + 1));
+	}
+
+	else
+	{
+		return (-1);
+	}
+}
+
+/**
+ * _sqrt_recursion - unction that returns the natural square root of a number
+ *
+ * @n: The intger
+ *
+ * Return: The square root
+*/
 
 int _sqrt_recursion(int n)
 {
-    
+	return (square(n, 1));
 }
