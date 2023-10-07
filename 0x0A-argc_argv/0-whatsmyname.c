6faf9823016ab7cc64a03d9e313ff51b60b6acb7
GMY96 <ag199612@gmail.com>
2023-10-08 02:15:56 +0300

    My argc, argv Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * @argc: The intger of varaibles
- *
- * @argv: An array of stings
- *
- * Return: Always 0
+* main - Entry point
+*
+* @argc: The intger of varaibles
+*
+* @argv: An array of stings
+*
+* Return: Always 0
 */
 
 int main(int argc, char *argv[])
 {
-    if (argc > 0)
-    {
-        printf("%s\n", argv[0]);
-    }
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
