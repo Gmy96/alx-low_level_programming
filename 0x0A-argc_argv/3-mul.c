d6d811a2aa7e9f3600a4bc0c598cee68b6f7fa15
GMY96 <ag199612@gmail.com>
2023-10-08 02:39:18 +0300

    My argc, argv Task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* @argc: The intger of varaibles
+*
+* @argv: An array of stings
+*
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+    int x, y;
+
+	if (argc != 3)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    else
+    {
+        x = atoi(argv[1]) * atoi(argv[2]);
+        printf("%d\n", x);
+        return (0);
+    }
+}
