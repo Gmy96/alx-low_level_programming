f2d12e442aca8f7b524bcddbc50e10cd2fcb57c0
GMY96 <ag199612@gmail.com>
2023-10-08 02:27:41 +0300

    My argc, argv Task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-/***/
+/**
+* main - entry point
+*
+* @argc: The intger of varaibles
+*
+* @argv: An array of stings
+*
+* Return: Always 0
+*/
 
 int main(int argc, char *argv[])
 {
-    if (argv[0])
-    {
-        printf("%d\n", argc - 1);
-    }
-    return (0);
+	if (argv[0])
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
 }
