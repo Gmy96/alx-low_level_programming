1c8d8464f60242142a0f92542d682798900f4dc7
GMY96 <ag199612@gmail.com>
2023-10-08 02:08:45 +0300

    My argc, argv Task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: The intger of varaibles
+ *
+ * @argv: An array of stings
+ *
+ * Retun: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
