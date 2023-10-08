f4c038582881bf3f36714e74baf8b2dbf01583cb
GMY96 <ag199612@gmail.com>
2023-10-08 03:43:29 +0300

    My argc, argv Task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,14 +19,9 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-		{
 			if (*c < '0' || *c > '9')
-			{
 				return (printf("Error\n"), 1);
-			}
-
-			x += atoi(argv[argc]);
-		}
+		x += atoi(argv[argc]);
 	}
 	printf("%d\n", x);
 
