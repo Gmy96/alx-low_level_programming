ac442b381bef6470ce52b69a8220ec20a73acfbd
GMY96 <ag199612@gmail.com>
2023-10-10 21:40:44 +0300

    My C - Hello, World Task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints exactly phase
+ *
+ * Return: 1
+*/
+
+int main(void)
+{
+	char pha[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, pha, 59);
+
+	return (1);
+}
