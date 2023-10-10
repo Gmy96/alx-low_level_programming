a22ba5a43a501906cbe75aec1a52fe1392474fa6
GMY96 <ag199612@gmail.com>
2023-10-10 05:44:25 +0300

    My More malloc, free Task

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -14,6 +14,7 @@ void *malloc_checked(unsigned int b)
 
 	if (x == 0)
 	{
+        free(x);
 		exit(98);
 	}
 
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/***/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    
+}
