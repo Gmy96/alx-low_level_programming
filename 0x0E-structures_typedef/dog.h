067ae23dcafb0c4c83b8a867adfbe9badec140d7
GMY96 <ag199612@gmail.com>
2023-10-12 02:23:11 +0300

    My Structures, typedef Task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - A structure with some elements
+*
+* Descrition: A structure for dogs data
+*
+* @name: Character for name
+*
+* @age: Float for age
+*
+* @owner: character for owner name
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
+
+#endif /* DOG_H */
